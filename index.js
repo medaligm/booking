7fdb3e797190bb92ae37bb0b386ebb28675b7282
medali <gmaghalgaouimohamedali@gmail.com>
2019-12-01 22:30:36 +0100

    mise a jour indexs

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/config")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`listening on port ${port}...`));
