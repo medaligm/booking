0bd926780b6aa58494f93f66a88e616993961d3c
medali <gmaghalgaouimohamedali@gmail.com>
2019-12-02 13:29:14 +0100

    error Handling

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 
 require("./startup/routes")(app);
 require("./startup/db")();
+require("./startup/logging")();
 require("./startup/config")();
 
 const port = process.env.PORT || 3000;
